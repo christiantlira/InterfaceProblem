b34e1b0e7fd94c5084444e302bcb68941a617b5d
Christian Torres <christiantlira@gmail.com>
2022-04-05 11:46:00 -0300

    Primeito commit do programa

---
+++ src/entities/Installment.java
@@ -0,0 +1,6 @@
+package entities;
+
+public class Installment {
+	private Date dueDate;
+	private Double amount;
+}
---
+++ src/model/services/InstallmentsService.java
@@ -0,0 +1,5 @@
+package model.services;
+
+public interface InstallmentsService {
+	double installments(double amount);
+}
